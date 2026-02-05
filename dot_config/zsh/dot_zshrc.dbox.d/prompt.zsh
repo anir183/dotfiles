da5c4cbfd1e25212e2fa8ef403aba2572e3ddfc4
anir183 <188557785+anir183@users.noreply.github.com>
2026-02-06 01:09:53 +0530

    minor(prompt): path relative to home instead of dir name

--- dot_config/zsh/dot_zshrc.dbox.d/prompt.zsh
+++ dot_config/zsh/dot_zshrc.dbox.d/prompt.zsh
@@ -4,4 +4,4 @@ setopt PROMPT_SUBST
 autoload -U colors && colors
 unset RPROMPT
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-export PROMPT='📦 %F{yellow}${CONTAINER_ID}%F{reset_color} %F{cyan}%1~%F{reset_color} $(__git_ps1 "(%s) ")%F{reset_color}%% '
+export PROMPT='📦 %F{yellow}${CONTAINER_ID}%F{reset_color} %F{cyan}%~%F{reset_color} $(__git_ps1 "(%s) ")%F{reset_color}%% '
