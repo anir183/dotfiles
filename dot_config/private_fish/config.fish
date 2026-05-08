0c5d08b77f62e5b23c3bcc283da428ec2fba9ae8
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-09 00:44:05 +0530

    config(fnm): init on start

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,9 @@ if status is-interactive
     # luarocks paths
     eval (luarocks path)
 
+    # fast node verison manager
+    fnm env --use-on-cd --shell fish | source
+
     # has to be near the end (zellij is the only exception)
     zoxide init fish --cmd chd | source
 
