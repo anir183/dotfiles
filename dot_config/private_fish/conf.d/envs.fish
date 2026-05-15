a41d1bf13813a2f79dc03848ee8d43efd26fb689
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-15 19:14:42 +0530

    config(zellij): minor env changes

--- dot_config/private_fish/conf.d/envs.fish
+++ dot_config/private_fish/conf.d/envs.fish
@@ -20,36 +20,42 @@ set -gx __fish_git_prompt_showcolorhints true
 set -gx __fish_git_prompt_char_stateseparator ' '
 
 #[[ environment variables for different software ]]
-set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
-set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
 
-set -gx CUDA_HOME /opt/cuda
-set -gx CUDA_CACHE_PATH $XDG_CACHE_HOME/nv
+set -gx GOPATH "$XDG_DATA_HOME/go"
 
-set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
+set -gx CUDA_HOME "/opt/cuda"
+set -gx CUDA_CACHE_PATH "$XDG_CACHE_HOME/nv"
+
+set -gx DOTNET_CLI_HOME "$XDG_DATA_HOME/dotnet"
+set -gx NUGET_PACKAGES "$XDG_CACHE_HOME/NuGetPackages"
+
+set -gx GNUPGHOME "$XDG_DATA_HOME/gnupg"
 
 if not is_distrobox
-	set -gx CHROME_EXCUTABLE /var/lib/flatpak/app/io.github.ungoogled_software.ungoogled_chromium/current/active/export/bin/io.github.ungoogled_software.ungoogled_chromium
+	set -gx CHROME_EXCUTABLE "/var/lib/flatpak/app/io.github.ungoogled_software.ungoogled_chromium/current/active/export/bin/io.github.ungoogled_software.ungoogled_chromium"
 end
 
-set -gx LUAROCKS_CONFIG $HOME/.config/luarocks/config.lua
+set -gx LUAROCKS_CONFIG "$HOME/.config/luarocks/config.lua"
 
-set -gx WGETRC $XDG_CONFIG_HOME/wgetrc
-set -gx WGET_HSTS_FILE $XDG_DATA_HOME/wget-hsts
+set -gx WGETRC "$XDG_CONFIG_HOME/wgetrc"
+set -gx WGET_HSTS_FILE "$XDG_DATA_HOME/wget-hsts"
 
-set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
+set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
 
 #[[ path var ]]
 # NOTE: fish_add_path sets another fish_user_path variables, performs some
 #       checks and operations and then adds them to path
 #       this causes changed ordering... use set -gx PATH if needs specific order
-fish_add_path --prepend $HOME/.local/luarocks/bin
-fish_add_path --prepend $XDG_DATA_HOME/npm/bin
-fish_add_path --prepend /opt/cuda/bin
+fish_add_path --prepend "$HOME/.local/lib/luarocks/bin"
+fish_add_path --prepend "$HOME/.local/lib/dotool-1.6"
+fish_add_path --prepend "$XDG_DATA_HOME/npm/bin"
+fish_add_path --prepend "/opt/cuda/bin"
 
 # NOTE: needs this order
 set -gx PATH \
-	$HOME/.local/bin \
-	$HOME/.local/scripts \
-	$XDG_DATA_HOME/nvim/mason/bin \
-	$PATH
+	"$HOME/.local/bin" \
+	"$HOME/.local/scripts" \
+	"$XDG_DATA_HOME/nvim/mason/bin" \
+	"$PATH"
